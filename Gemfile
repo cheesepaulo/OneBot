09e511b03d581b58d3283889f2d6178640157643
Paulo Cesar <pcdsjr@gmail.com>
2017-05-20 16:43:14 -0500

    prepare database and seeds for deploy

--- Gemfile
+++ Gemfile
@@ -5,11 +5,13 @@ gem "activerecord"
 gem "sinatra-activerecord"
 gem 'pg'
 gem 'rake'
-gem 'pg_search' 
+gem 'pg_search'
+
 
 group :development do
  gem "tux"
 end
+
 
 group :test do
   gem "rack-test", require: "rack/test"
--- config/database.rb
+++ config/database.rb
@@ -1,32 +1,34 @@
 configure :test do
-   set :database, {
-     adapter: 'postgresql',
-     encoding: 'utf8',
-     database: 'onebot_test',
-     pool: 5,
-     username: 'postgres',
-     host: 'postgres'
-   }
+  set :database, {
+    adapter: 'postgresql',
+    encoding: 'utf8',
+    database: 'onebot_test',
+    pool: 5,
+    username: 'postgres',
+    host: 'postgres'
+  }
 end
 
 configure :development do
- set :database, {
-   adapter: 'postgresql',
-   encoding: 'utf8',
-   database: 'onebot_development',
-   pool: 5,
-   username: 'postgres',
-   host: 'postgres'
- }
+  set :database, {
+    adapter: 'postgresql',
+    encoding: 'utf8',
+    database: 'onebot_development',
+    pool: 5,
+    username: 'postgres',
+    host: 'postgres'
+  }
 end
 
 configure :production do
-   set :database, {
-     adapter: 'postgresql',
-     encoding: 'utf8',
-     database: 'onebot_production',
-     pool: 5,
-     username: 'postgres',
-     host: 'postgres'
-   }
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+
+  set :database, {
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  }
 end
