58a140a27e65d8d5047ca267a3c569245abe13c5
Paulo Cesar <pcdsjr@gmail.com>
2017-11-14 20:38:17 -0500

    rename factory_girl to factory_bot

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,10 +1,10 @@
-require 'factory_girl'
+require 'factory_bot'
 
 # RSpec without Rails
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    FactoryGirl.find_definitions
+    FactoryBot.find_definitions
   end
 end
